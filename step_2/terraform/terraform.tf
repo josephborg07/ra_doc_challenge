c89fca3135fc0844d71ccb18659c9ae8797197af
Joseph Borg <joseph.borg@authenticgaming.com>
2021-11-30 08:06:01 +0100

    updated terraform to save state in s3

--- step_2/terraform/terraform.tf
+++ step_2/terraform/terraform.tf
@@ -3,3 +3,11 @@ provider "aws" {
   access_key = "AKIAQWBDCKAHAXX5DZS4"
   secret_key = "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"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "doc_tf"
+    key    = "doc_share_state"
+    region = "us-east-1"
+  }
+}
